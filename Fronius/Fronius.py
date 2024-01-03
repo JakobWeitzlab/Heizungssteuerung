137f11810d38cde136897bcf1fdbbae8b42c2f38
JakobWeitzlab <jakokb.weitzlab1@gmx.at>
2024-01-03 19:10:16 +0100

    +exception handling +sendString update

--- Fronius/Fronius.py
+++ Fronius/Fronius.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import logging
+#import traceback
 
 def Get_Fronius_Data(urls, searchKeys):
     values = []
@@ -11,16 +13,20 @@ def Get_Fronius_Data(urls, searchKeys):
             #Grab apropriate value
             if isinstance(searchKeys, list):
                 for sk in searchKeys:
-                    values.append(Get_Values_From_Response(sk, apiResponseJson))
+                    values.append(str(sk)+","+str(Get_Values_From_Response(sk, apiResponseJson)))
             else:
-                values.append(Get_Values_From_Response(searchKeys, apiResponseJson))
-
-            #ActualPower = apiResponseJson["Body"]["Data"]["Inverters"]["1"]["P"]
-            #DayPower = apiResponseJson["Body"]["Data"]["Inverters"]["1"]["E_Day"]
+                values.append(str(searchKeys)+","+str(Get_Values_From_Response(searchKeys, apiResponseJson)))
 
             return values
+    
         except requests.exceptions.Timeout:
             print("Wechselrichter: ", url, "ist offline")
+            startIndex = url.find('://')
+            endIndex = url.find('/', startIndex+3)
+            address = url[startIndex+3:endIndex]
+            pass
+        except Exception as e:
+            logging.error(e)
             
 def Get_Values_From_Response(searchKeys, inputDic):
     if searchKeys in inputDic:
@@ -33,3 +39,7 @@ def Get_Values_From_Response(searchKeys, inputDic):
                 return found
     return None
 
+demo = False
+if demo:
+    temp = Get_Fronius_Data(["http://192.168.0.36/solar_api/v1/GetPowerFlowRealtimeData.fcgi", "http://192.168.0.36/solar_api/v1/GetPowerFlowRealtimeData.fcgi"],["E_Day","P"])
+    print(temp)
